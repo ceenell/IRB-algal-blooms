677b5f9362c16f142579d26d2291e5fdd9d77418
Platt <lplatt@usgs.gov>
2022-06-30 16:19:18 -0500

    delete unnecessary `ggdist`

--- data-pipeline/3_visualize/src/generate_timeseries_figure.R
+++ data-pipeline/3_visualize/src/generate_timeseries_figure.R
@@ -17,7 +17,7 @@ generate_dist_ts_figure <- function(file_out, data_in) {
   p <- data_in %>%
     arrange(date) %>%
     ggplot(aes(x = date, y = mean_result, ymin = minimum_result, ymax = maximum_result)) +
-    ggdist::geom_lineribbon(aes(color = site_no, fill = site_no), alpha=0.4) +
+    geom_lineribbon(aes(color = site_no, fill = site_no), alpha=0.4) +
     facet_grid(param_grp ~ ., scales = 'free') +
     theme_bw()
   ggsave(file_out, p, width = 7, height = 10)
