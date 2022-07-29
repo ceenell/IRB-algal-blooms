a8c101c199f69fab6470f7380f7cd5ae3ea55e59
Cee Nell <cnell@usgs.gov>
2022-07-29 13:24:32 -0500

    Update data-pipeline/1_fetch/src/get_nlcd_aoi.R
    Co-authored-by: Lindsay (Carr) Platt <lplatt@usgs.gov>

--- data-pipeline/1_fetch/src/get_nlcd_aoi.R
+++ data-pipeline/1_fetch/src/get_nlcd_aoi.R
@@ -2,8 +2,6 @@ get_nlcd_aoi <- function(aoi, aoi_label = 'drb', nlcd_dataset = 'landcover',
                          nlcd_year, file_name, out_folder){
   
   print(nlcd_year)
-
-  # +proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs 
   ## this will give us the nlcd data for the extent of the aoi 
   raster_aoi <- get_nlcd(template = aoi, label = aoi_label, year = nlcd_year , dataset = nlcd_dataset) 
   
