c22b7e6a07809dbdc3c404c3306fe55e2cacd913
Cee Nell <cnell@usgs.gov>
2022-07-29 13:34:24 -0500

    merging

--- data-pipeline/1_fetch/src/get_nlcd_aoi.R
+++ data-pipeline/1_fetch/src/get_nlcd_aoi.R
@@ -5,9 +5,6 @@ get_nlcd_aoi <- function(aoi, aoi_label = 'drb', nlcd_dataset = 'landcover',
   ## this will give us the nlcd data for the extent of the aoi 
   raster_aoi <- get_nlcd(template = aoi, label = aoi_label, year = nlcd_year , dataset = nlcd_dataset) 
   
-  ## mask to aoi
-  #mask_raster_aoi <- raster::mask(raster_aoi, aoi)
-  
   path <- file.path(out_folder, file_name)
   out_raster <- writeRaster(raster_aoi, filename = path, format = 'GTiff', overwrite = TRUE)
   
