b9610a540e694abc07ac257a3ad427aa64c309cd
Platt <lplatt@usgs.gov>
2022-08-08 15:50:22 -0500

    fancy gradient rectangle around a bloom

--- finesse.R
+++ finesse.R
@@ -26,22 +26,28 @@ fchl_last <- fchl_data %>% filter(date == max(date))
 do_last <- do_data %>% filter(date == max(date))
 
 bloom_date <- as.Date('2020-06-25')
+ndays <- 5
+gradient_rect <- tibble(
+  xleft = c(seq(bloom_date-ndays, bloom_date, by = 1), seq(bloom_date+1, bloom_date+ndays, by = 1))
+) %>%
+  mutate(xright = xleft + 1) %>%
+  mutate(alpha_val = as.numeric(1-((bloom_date - xleft)/ndays))) %>%
+  # Mirror the alphas
+  mutate(alpha_val = ifelse(alpha_val > 1, 2 - alpha_val, alpha_val))
 
-p_fchl <- ggplot(fchl_data, aes(x = date,
-                                y = mean_result,
-                                ymin = minimum_result,
-                                ymax = maximum_result)) +
-  geom_text(data = fchl_last, aes(label = site_label, color = site_label),
+p_fchl <- ggplot(data = fchl_data) +
+  geom_text(data = fchl_last, aes(x = date, y = mean_result, label = site_label, color = site_label),
             fontface ="bold", hjust = 0, nudge_x = 0.5) +
   geom_text(x = as.Date('2020-05-01'), y = 107,
             label = 'Mean daily algal fluorescence, ug/L',
             color = '#918b8b', size = 5, hjust = 0.10) +
-  # Copy this same styling to the DO plot
-  # ggdist::geom_lineribbon(aes(color = site_label, fill = site_label), alpha=0.25) +
-  geom_rect(xmin = bloom_date - 5, xmax = bloom_date + 5,
+  geom_rect(data = gradient_rect, aes(xmin = xleft, xmax = xright, alpha = alpha_val),
             ymin = -Inf, ymax = Inf,
-            fill = '#dedcdc', alpha = 0.10) +
-  geom_line(aes(color = site_label), size = 1) +
+            fill = '#dedcdc') +
+  # Copy this same styling to the DO plot
+  # ggdist::geom_lineribbon(aes(ymin = minimum_result, ymax = maximum_result,
+  #                             color = site_label, fill = site_label), alpha=0.25) +
+  geom_line(aes(x = date, y = mean_result, color = site_label), size = 1) +
   scale_color_manual(values = c("#506992", "#5FB5D1", "#ABD7C8"),
                      breaks = c("Henry", "Starved Rock", "Seneca")) +
   # scale_fill_manual(values = c("#506992", "#5FB5D1", "#ABD7C8"),
@@ -57,24 +63,23 @@ p_fchl <- ggplot(fchl_data, aes(x = date,
         axis.text.y = element_text(face = 'bold', color = '#bdb9b9', vjust=0.3, hjust=1),
         axis.ticks = element_blank()) +
   coord_cartesian(clip = 'off') +
-  xlim(c(min(data_ready$date), max(data_ready$date) + 1))
+  xlim(c(min(data_ready$date), max(data_ready$date) + 1)) +
+  ylim(c(min(fchl_data$minimum_result), max(fchl_data$maximum_result)))
 
-p_do <- ggplot(do_data, aes(x = date,
-                            y = mean_result,
-                            ymin = minimum_result,
-                            ymax = maximum_result)) +
-  geom_text(data = do_last, aes(label = site_label, color = site_label),
+p_do <- ggplot(do_data) +
+  geom_text(data = do_last, aes(x = date, y = mean_result, label = site_label, color = site_label),
             fontface ="bold", hjust = 0, nudge_x = 0.5) +
   geom_text(x = as.Date('2020-05-01'), y = 17,
             label = 'Mean daily dissolved oxygen, mg/L',
             color = '#918b8b', size = 5, hjust = 0.10) +
+  geom_rect(data = gradient_rect, aes(xmin = xleft, xmax = xright, alpha = alpha_val),
+            ymin = -Inf, ymax = Inf,
+            fill = '#dedcdc') +
   # Below this comment was copied from fchl plot
   # Except for turning off the x-axis labels
-  # ggdist::geom_lineribbon(aes(color = site_label, fill = site_label), alpha=0.25) +
-  geom_rect(xmin = bloom_date - 5, xmax = bloom_date + 5,
-            ymin = -Inf, ymax = Inf,
-            fill = '#dedcdc', alpha = 0.10) +
-  geom_line(aes(color = site_label), size = 1) +
+  # ggdist::geom_lineribbon(aes(ymin = minimum_result, ymax = maximum_result,
+  #                             color = site_label, fill = site_label), alpha=0.25) +
+  geom_line(aes(x = date, y = mean_result, color = site_label), size = 1) +
   scale_color_manual(values = c("#506992", "#5FB5D1", "#ABD7C8"),
                      breaks = c("Henry", "Starved Rock", "Seneca")) +
   # scale_fill_manual(values = c("#506992", "#5FB5D1", "#ABD7C8"),
@@ -91,7 +96,8 @@ p_do <- ggplot(do_data, aes(x = date,
         axis.text.y = element_text(face = 'bold', color = '#bdb9b9', vjust=0.3,hjust=1),
         axis.ticks = element_blank()) +
   coord_cartesian(clip = 'off') +
-  xlim(c(min(data_ready$date), max(data_ready$date) + 1))
+  xlim(c(min(data_ready$date), max(data_ready$date) + 1)) +
+  ylim(c(min(do_data$minimum_result), max(do_data$maximum_result)))
 
 p_combined <- cowplot::plot_grid(p_do, p_fchl, nrow=2)
 ggsave('habs_fancy_ts.png', p_combined, width = 15, height = 8)
